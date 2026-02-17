41b8976d591c6e2d0132207438199b68a945a369
Maria Cadavid <maria.cadavid@concentrix.com>
2026-02-16 21:28:36 -0500

    added branch controller and completed angular form integration

---
+++ AmarisScheduleAppointments/Controllers/BranchController.cs
@@ -0,0 +1,26 @@
+﻿using AmarisScheduleAppointments.Infrastructure.DataBase;
+using AmarisScheduleAppointments.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AmarisScheduleAppointments.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BranchController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public BranchController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Branch>>> GetBranches()
+        {
+            var branches = await _context.Branches.ToListAsync();
+            return Ok(branches);
+        }
+    }
+}
